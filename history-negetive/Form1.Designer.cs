8d31e2d9f85324259496fca5d928a23f4f307af7
The-Black-knight-2002 <math.mohammad.arab@gmail.com>
2021-12-01 19:40:47 +0330

    Add project files.

---
+++ history-negetive/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace history_negetive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.comboBeginDateMonth = new System.Windows.Forms.ComboBox();
+            this.comboBeginDateDay = new System.Windows.Forms.ComboBox();
+            this.comboBeginDateYear = new System.Windows.Forms.ComboBox();
+            this.comboEndDateMonth = new System.Windows.Forms.ComboBox();
+            this.comboEndDateDay = new System.Windows.Forms.ComboBox();
+            this.comboEndDateYear = new System.Windows.Forms.ComboBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Begin Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "End Date";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(275, 230);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 17);
+            this.labelResult.TabIndex = 2;
+            // 
+            // comboBeginDateMonth
+            // 
+            this.comboBeginDateMonth.FormattingEnabled = true;
+            this.comboBeginDateMonth.Location = new System.Drawing.Point(186, 95);
+            this.comboBeginDateMonth.Name = "comboBeginDateMonth";
+            this.comboBeginDateMonth.Size = new System.Drawing.Size(121, 24);
+            this.comboBeginDateMonth.TabIndex = 3;
+            // 
+            // comboBeginDateDay
+            // 
+            this.comboBeginDateDay.FormattingEnabled = true;
+            this.comboBeginDateDay.Location = new System.Drawing.Point(339, 95);
+            this.comboBeginDateDay.Name = "comboBeginDateDay";
+            this.comboBeginDateDay.Size = new System.Drawing.Size(121, 24);
+            this.comboBeginDateDay.TabIndex = 4;
+            // 
+            // comboBeginDateYear
+            // 
+            this.comboBeginDateYear.FormattingEnabled = true;
+            this.comboBeginDateYear.Location = new System.Drawing.Point(495, 95);
+            this.comboBeginDateYear.Name = "comboBeginDateYear";
+            this.comboBeginDateYear.Size = new System.Drawing.Size(121, 24);
+            this.comboBeginDateYear.TabIndex = 5;
+            // 
+            // comboEndDateMonth
+            // 
+            this.comboEndDateMonth.FormattingEnabled = true;
+            this.comboEndDateMonth.Location = new System.Drawing.Point(186, 137);
+            this.comboEndDateMonth.Name = "comboEndDateMonth";
+            this.comboEndDateMonth.Size = new System.Drawing.Size(121, 24);
+            this.comboEndDateMonth.TabIndex = 6;
+            // 
+            // comboEndDateDay
+            // 
+            this.comboEndDateDay.FormattingEnabled = true;
+            this.comboEndDateDay.Location = new System.Drawing.Point(339, 137);
+            this.comboEndDateDay.Name = "comboEndDateDay";
+            this.comboEndDateDay.Size = new System.Drawing.Size(121, 24);
+            this.comboEndDateDay.TabIndex = 7;
+            // 
+            // comboEndDateYear
+            // 
+            this.comboEndDateYear.FormattingEnabled = true;
+            this.comboEndDateYear.Location = new System.Drawing.Point(495, 137);
+            this.comboEndDateYear.Name = "comboEndDateYear";
+            this.comboEndDateYear.Size = new System.Drawing.Size(121, 24);
+            this.comboEndDateYear.TabIndex = 8;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(278, 307);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(182, 79);
+            this.buttonCalculate.TabIndex = 9;
+            this.buttonCalculate.Text = "Calculate Difference";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.comboEndDateYear);
+            this.Controls.Add(this.comboEndDateDay);
+            this.Controls.Add(this.comboEndDateMonth);
+            this.Controls.Add(this.comboBeginDateYear);
+            this.Controls.Add(this.comboBeginDateDay);
+            this.Controls.Add(this.comboBeginDateMonth);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Time Difference Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.ComboBox comboBeginDateMonth;
+        private System.Windows.Forms.ComboBox comboBeginDateDay;
+        private System.Windows.Forms.ComboBox comboBeginDateYear;
+        private System.Windows.Forms.ComboBox comboEndDateMonth;
+        private System.Windows.Forms.ComboBox comboEndDateDay;
+        private System.Windows.Forms.ComboBox comboEndDateYear;
+        private System.Windows.Forms.Button buttonCalculate;
+    }
+}
+
---
+++ history-negetive/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace history_negetive
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBeginDateMonth.Tag = "Begin";
+            comboBeginDateYear.Tag  = "Begin";
+            comboEndDateMonth.Tag   = "End";
+            comboEndDateYear.Tag    = "End";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Definition of months
+            string[] months =
+            {
+                "January"  ,   "February",  "March"   ,    "April" ,
+                "May"      ,   "June"    ,  "July"    ,    "August",
+                "September",   "October" ,  "November",    "December"
+            };
+
+            //Fill combo boxes with months
+            for (int i = 0; i < 12; i++)
+            {
+                comboBeginDateMonth.Items.Add(months[i]);
+                comboEndDateMonth.Items.Add(months[i]);
+            }
+
+            //Default January
+            comboBeginDateMonth.SelectedIndex = 0;
+            comboEndDateMonth.SelectedIndex = 0;
+
+            //Fill combo boxes with years
+            for (int i = 0; i <=DateTime.Now.Year; i++)
+            {
+                comboBeginDateYear.Items.Add(String.Format("{0:0000}", i));
+                comboEndDateYear.Items.Add(String.Format("{0:0000}", i));
+            }
+
+            //Put default year on 2000
+            comboBeginDateYear.SelectedIndex = 1999;
+            comboEndDateYear.SelectedIndex = 1999;
+
+            //Fill combo boxes for days
+            FillDays(comboBeginDateDay, "Begin");
+            FillDays(comboEndDateDay, "End");
+
+            //Add event controller
+            comboBeginDateMonth.SelectedIndexChanged += new EventHandler(RecalculateDays);
+            comboBeginDateYear.SelectedIndexChanged  += new EventHandler(RecalculateDays);
+            comboEndDateMonth.SelectedIndexChanged   += new EventHandler(RecalculateDays);
+            comboEndDateDay.SelectedIndexChanged     += new EventHandler(RecalculateDays);
+        }
+        private void FillDays(ComboBox comboBox, string tag)
+        {
+            int selectedMonth;
+            int selectedYear;
+
+            if (tag == "Begin") 
+            {
+                selectedMonth = comboBeginDateMonth.SelectedIndex + 1;
+                selectedYear  = comboBeginDateYear.SelectedIndex + 1;
+            }
+            else
+            {
+                selectedMonth = comboEndDateMonth.SelectedIndex + 1;
+                selectedYear  = comboEndDateYear.SelectedIndex + 1;
+            }
+
+            int maxDay = DateTime.DaysInMonth(selectedYear, selectedMonth);
+
+            comboBox.Items.Clear();
+
+            for (int i = 0; i <= maxDay; i++)
+            {
+                comboBox.Items.Add(i);
+            }
+
+            comboBox.SelectedIndex = 0;
+        }
+        private void RecalculateDays(object sender, EventArgs e)
+        {
+            if ((sender as ComboBox).Tag.ToString() == "Begin")
+            {
+                FillDays(comboBeginDateDay, "Begin");
+            }
+            else
+            {
+                FillDays(comboEndDateDay, "end");
+            }
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            DateTime beginDate = new DateTime
+                (
+                comboBeginDateYear.SelectedIndex + 1,
+                comboBeginDateMonth.SelectedIndex + 1,
+                comboBeginDateDay.SelectedIndex + 1
+                );
+
+            DateTime endDate = new DateTime
+                (
+                comboEndDateDay.SelectedIndex + 1,
+                comboEndDateMonth.SelectedIndex + 1,
+                comboEndDateYear.SelectedIndex + 1
+                );
+
+            TimeSpan difference = endDate.Subtract(beginDate);
+
+            labelResult.Text = string.Format("{0} Days", difference.Days);
+        }
+    }
+}
